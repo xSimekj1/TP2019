797c7193512c87f7f3d7cf3b3ac7d8710aa50d07
xsimekj <jakapo.simek95@gmail.com>
2019-10-20 15:46:00 +0200

    init structure routing

--- TPFE/src/app/app-routing.module.ts
+++ TPFE/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'O-projekte', component: AboutComponent },
+  { path: 'Team', component: TeamComponent },
+  { path: 'Stretnutia', component: MeetingsComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TPFE/src/app/app.module.ts
+++ TPFE/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { TeamComponent } from './team/team.component';
 import { MeetingsComponent } from './meetings/meetings.component';
 import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamComponent,
     MeetingsComponent,
-    AboutComponent
+    AboutComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
