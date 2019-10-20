8a229537ea96bb4b8d4e4bb73ea0ee543ae71868
xsimekj <jakapo.simek95@gmail.com>
2019-10-20 15:40:50 +0200

    init base structure

--- TPFE/src/app/app.module.ts
+++ TPFE/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent,
+    MeetingsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
