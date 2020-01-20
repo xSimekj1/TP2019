3d6efffc5b3d3a9262185050aaeb9d748c3c68ba
xsimekj <jakapo.simek95@gmail.com>
2020-01-20 19:57:00 +0100

    add meeting documents

--- TPFE/src/app/meetings/meetings.component.ts
+++ TPFE/src/app/meetings/meetings.component.ts
@@ -6,18 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meetings.component.scss']
 })
 export class MeetingsComponent implements OnInit {
+  openedMeeting;
+
   meetings = [
     {id: 1, name: 'Zápisnica 1', img: './assets/Zapisnica_c1.pdf'},
     {id: 2, name: 'Zápisnica 2', img: './assets/Zapisnica_c2.pdf'},
     {id: 3, name: 'Zápisnica 3', img: './assets/Zapisnica_c3.pdf'},
+    {id: 4, name: 'Zápisnica 4', img: './assets/Zapisnica_c4.pdf'},
+    {id: 5, name: 'Zápisnica 5', img: './assets/Zapisnica_c5.pdf'},
+    {id: 6, name: 'Zápisnica 6', img: './assets/Zapisnica_c6.pdf'},
+    {id: 7, name: 'Zápisnica 7', img: './assets/Zapisnica_c7.pdf'},
+    {id: 8, name: 'Zápisnica 8', img: './assets/Zapisnica_c8.pdf'},
+    {id: 9, name: 'Zápisnica 9', img: './assets/Zapisnica_c9.pdf'},
+    {id: 10, name: 'Zápisnica 10', img: './assets/Zapisnica_c10.pdf'},
   ];
 
   constructor() { }
 
   ngOnInit() {
+    this.openedMeeting = this.meetings[0];
   }
 
   openDoc(docId) {
+    this.openedMeeting = this.meetings.find(meeting => meeting.id === docId);
   }
 
 }
